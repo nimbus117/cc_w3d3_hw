5ce89e9216243377388b5ae4279d8eb1b6c34ad5
nimbus117 <mail@nimbus117.co.uk>
2018-08-15 14:47:18 +0100

    added artist delete all method

--- models/artist.rb
+++ models/artist.rb
@@ -1,21 +1,30 @@
 require_relative('../db/sqlrunner.rb')
 
+
+
 class Artist
   def initialize(options)
     @id = options['id'].to_i if options['id']
     @name = options['name']
   end
 
-  def save()
+  def save
     sql = "
       INSERT INTO artists
         (name)
       VALUES
         ($1)
-      RETURNING *;
+      RETURNING id;
     "
     values = [@name]
     @id = SqlRunner.run(sql, values)[0]['id'].to_i
+  end
+
+  def Artist.delete_all
+    sql = "
+      DELETE FROM artists;
+    "
+    SqlRunner.run(sql)
   end
 
 end
