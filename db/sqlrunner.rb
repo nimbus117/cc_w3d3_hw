2ed7e4869bd9842d4f795cf374a0e010fa2183d6
nimbus117 <mail@nimbus117.co.uk>
2018-08-15 19:49:43 +0100

    updated PG connection string to work on ubuntu, still works on mac

--- db/sqlrunner.rb
+++ db/sqlrunner.rb
@@ -3,7 +3,7 @@ require('pg')
 class SqlRunner
 
   def SqlRunner.run(sql, values = [])
-    db = PG.connect({dbname: 'music_collection', host: 'localhost'})
+    db = PG.connect({dbname: 'music_collection', host: ''})
     db.prepare('run', sql)
     result = db.exec_prepared('run', values)
     db.close
