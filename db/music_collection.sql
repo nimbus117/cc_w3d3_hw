f84b6a4c774fc82f4aee1afcc29e74211a2e7fbc
nimbus117 <mail@nimbus117.co.uk>
2018-08-16 09:04:29 +0100

    added NOT NULL to release_date field

--- db/music_collection.sql
+++ db/music_collection.sql
@@ -10,6 +10,6 @@ CREATE TABLE albums(
 	id SERIAL8 PRIMARY KEY,
 	title VARCHAR(255) NOT NULL,
 	genre VARCHAR(255) NOT NULL,
-	release_date DATE,
+	release_date DATE NOT NULL,
 	artist_id INT8 REFERENCES artists(id)
 );
